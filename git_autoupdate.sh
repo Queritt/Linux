292777fd12f074434e0d77e1c4676ddc891662da
Queritt <42835693+Queritt@users.noreply.github.com>
2023-06-19 14:50:17 +0300

    Update git_autoupdate.sh
    Delete test information.

--- git_autoupdate.sh
+++ git_autoupdate.sh
@@ -20,11 +20,11 @@ LAST_UPDATE=`git show --no-notes --format=format:"%H" $BRANCH | head -n 1`
 LAST_COMMIT=`git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 1`
 
 # IF we don't match we should update local
-if [ $LAST_COMMIT = $LAST_UPDATE ]; then
+if [ $LAST_COMMIT != $LAST_UPDATE ]; then
         echo "Updating your branch $BRANCH"
         git pull --no-edit
-        MSG=$(git log -1  --pretty=format:'%cn, %cd, %s' | grep -v "^$")
-        echo -e "Update information:\n$MSG" | mail -v -s "Git repository \"$REPO\" updated." $MAIL
+        MSG=$(git log -1 --pretty=format:'%cn, %cd, %s' | grep -v "^$")
+        echo -e "Update information:\n$MSG" | mail -s "Git repository \"$REPO\" updated." $MAIL
 
 else
         echo "No updates available"
